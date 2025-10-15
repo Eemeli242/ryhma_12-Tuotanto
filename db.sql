49d6d6f4c20f2c79902052e1c5a741c7ebaee5de
Eemeli242 <nyyhhd79@gmail.com>
2025-10-15 19:41:26 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2025 at 12:21 PM
+-- Generation Time: Oct 15, 2025 at 06:39 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -55,7 +55,9 @@ INSERT INTO `bookings` (`id`, `customer_id`, `cabin_id`, `customer_name`, `custo
 (30, 16, 3, 'Topi', 'topimail@gmail.com', '2025-11-13', '2025-11-28', 2, '2025-10-10 10:18:14', 2250.00, 'paid', NULL, NULL),
 (31, 16, 6, 'Topi', 'topimail@gmail.com', '2025-10-10', '2025-10-31', 1, '2025-10-10 10:18:37', 2100.00, 'paid', NULL, NULL),
 (32, 1, 3, 'Eemeli Määttä', 'eemelimail@gmail.com', '2025-10-10', '2025-10-31', 1, '2025-10-10 10:20:39', 3150.00, 'paid', NULL, NULL),
-(33, 1, 5, 'Eemeli Määttä', 'eemelimail@gmail.com', '2025-10-10', '2025-10-31', 1, '2025-10-10 10:21:17', 2730.00, 'paid', NULL, NULL);
+(33, 1, 5, 'Eemeli Määttä', 'eemelimail@gmail.com', '2025-10-10', '2025-10-31', 1, '2025-10-10 10:21:17', 2730.00, 'paid', NULL, NULL),
+(34, 15, 1, 'Kim', 'kimmail@gmail.com', '2026-06-01', '2026-06-02', 1, '2025-10-12 09:37:17', 120.00, 'paid', NULL, NULL),
+(35, 1, 3, 'Eemeli Määttä', 'eemelimail@gmail.com', '2026-11-01', '2026-11-03', 2, '2025-10-12 10:12:07', 300.00, 'cancelled', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -87,8 +89,8 @@ INSERT INTO `cabins` (`id`, `owner_id`, `name`, `description`, `price_per_night`
 (4, 0, 'Siniranta', 'Siniranta – Kirkasvetisen järven rannalla, jossa vesi heijastaa taivaan syvänsinisen sävyn kesäpäivinä.adaadadddd', 200.00, 6, '/uploads/1760091197_hero-huvilakategoria1-min.jpg', 'Jyväskylä', NULL, 1),
 (5, 0, 'Karhunpesä', 'Karhunpesä – Vankka ja lämmin hirsimökki, joka huokuu erämaan voimaa ja kutsuu talvi-iltoina takkatulen äärelle.', 130.00, 4, 'images/cabin5.jpg', 'Turku', NULL, 1),
 (6, 0, 'Lumilinna', 'Lumilinna – Talven taikaa rakastavalle: mökki, joka peittyy kauniiseen lumipeitteeseen ja loistaa kynttilälyhtyjen valossa.', 100.00, 4, 'images/cabin6.jpg', 'Jyväskylä', 1, 1),
-(14, 1, 'Kesäheinä', 'Kesäheinä – Avoimella niityllä sijaitseva mökki, jonka ympärillä tuoksuvat luonnonkukat ja heinäladot.', 50.00, 11, 'images/cabin7.jpg', 'Hämeenlinna', NULL, 1),
-(15, 1, 'Kallioranta', 'Kallioranta – Jyrkän kalliorannan päällä lepäävä paikka, josta voi hypätä suoraan kirkkaaseen veteen. Lämmin järvivesi.', 111.00, 2, 'images/cabin8.jpg', 'Hämeenlinna', NULL, 1),
+(14, 1, 'Kesäheinä', 'Kesäheinä – Avoimella niityllä sijaitseva mökki, jonka ympärillä tuoksuvat luonnonkukat ja heinäladot.', 20.00, 11, 'images/cabin7.jpg', 'Helsinki', NULL, 1),
+(15, 1, 'Kallioranta', 'Kallioranta – Jyrkän kalliorannan päällä lepäävä paikka, josta voi hypätä suoraan kirkkaaseen veteen. Lämmin järvivesi.', 112.00, 2, 'images/cabin8.jpg', 'Hämeenlinna', NULL, 1),
 (22, 1, 'Kalakukko', 'Villa Kalakukko on tunnelmallinen ja modernisti varusteltu hirsimökki, joka sijaitsee rauhallisella paikalla kirkasvetisen järven rannalla. ', 11.00, 11, '/uploads/1760091280_Mökki-110-kesäulko-2-scaled-e1588589079801.jpg', 'Kuopio', NULL, 1);
 
 -- --------------------------------------------------------
@@ -119,7 +121,10 @@ INSERT INTO `reviews` (`id`, `booking_id`, `cabin_id`, `user_id`, `rating`, `com
 (27, 30, 3, 16, 4, 'Hyvä mökki voin suositella.', '2025-10-10 10:19:09', 1),
 (28, 29, 1, 16, 1, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\r\n\r\nHUONO MÖKKI', '2025-10-10 10:19:16', 1),
 (29, 31, 6, 16, 2, 'Siisti mökki mutta kallis', '2025-10-10 10:19:23', 1),
-(30, 32, 3, 1, 5, 'Hieno mökki. En voi muutakuin suositella.', '2025-10-10 10:20:53', 1);
+(30, 32, 3, 1, 5, 'Hieno mökki. En voi muutakuin suositella.', '2025-10-10 10:20:53', 1),
+(31, 34, 1, 15, 5, 'mahtavaa', '2025-10-12 09:37:24', 1),
+(32, 33, 5, 1, 4, 'Oli hyvä', '2025-10-12 09:54:47', 1),
+(33, 35, 3, 1, 3, 'Hyvä mökki', '2025-10-12 10:12:53', 1);
 
 -- --------------------------------------------------------
 
@@ -144,9 +149,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `phone`, `profile_image`, `password`, `created_at`, `balance`, `role`) VALUES
-(1, 'Eemeli', 'eemelimail@gmail.com', '04049027931', 'uploads/1760091145_1200x630cw-removebg-preview.png', '$2y$10$F0B8XZPqtzZYT6S0.F.B3./MGP3TcsuoM3oG7XAMEjQrJDX06WoaG', '2025-09-15 10:43:23', 5462.00, 'admin'),
+(1, 'Eemeli', 'eemelimail@gmail.com', '04049027931', 'uploads/1760091145_1200x630cw-removebg-preview.png', '$2y$10$F0B8XZPqtzZYT6S0.F.B3./MGP3TcsuoM3oG7XAMEjQrJDX06WoaG', '2025-09-15 10:43:23', 5162.00, 'admin'),
 (14, 'Samu', 'samumail@gmail.com', '0450002085', 'images/avatar.jpg', '$2y$10$ZpWLP4euK4sySvwiZoGiFuxIsWIWAfIcfJTAE.0tRlnXDyNjd/yZW', '2025-10-10 10:00:53', 698.00, 'user'),
-(15, 'Kim', 'kimmail@gmail.com', '04400295972', 'images/avatar.jpg', '$2y$10$b19bkCTQ2ZtcqU0.Inz7Fe5UCeKl2jyKpe/1c.UTCJBY.YoknoaZe', '2025-10-10 10:01:08', 400.00, 'user'),
+(15, 'Kim', 'kimmail@gmail.com', '04400295972', 'images/avatar.jpg', '$2y$10$b19bkCTQ2ZtcqU0.Inz7Fe5UCeKl2jyKpe/1c.UTCJBY.YoknoaZe', '2025-10-10 10:01:08', 280.00, 'user'),
 (16, 'Topi', 'topimail@gmail.com', '04003997881', 'images/avatar.jpg', '$2y$10$tAlUi5vKmjZLEAuhSZXWn.EnrBu.v5JG6FLZSso8He.LSrhJw8ORK', '2025-10-10 10:01:40', 14810.00, 'user');
 
 --
@@ -194,19 +199,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `cabins`
 --
 ALTER TABLE `cabins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `users`
